85a1a24cd0d039bf98b4e7ff3dfee8dd3d8d1dea
Artur Kuklenko <kuklenko2020@gmail.com>
2026-06-09 21:56:38 +0300

    Refactor ImageService

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -17,20 +19,17 @@ import java.io.IOException;
 public class ImageServiceImpl implements ImageService {
 
 
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
-    public ImageServiceImpl( RecipeRepository recipeService) {
+    public ImageServiceImpl( RecipeReactiveRepository recipeReactiveRepository) {
 
-        this.recipeRepository = recipeService;
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
     @Override
     @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
-
+    public Mono<Recipe> saveImageFile(String recipeId, MultipartFile file) {
         try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
-
             Byte[] byteObjects = new Byte[file.getBytes().length];
 
             int i = 0;
@@ -38,15 +37,23 @@ public class ImageServiceImpl implements ImageService {
             for (byte b : file.getBytes()){
                 byteObjects[i++] = b;
             }
-
-            recipe.setImage(byteObjects);
-
-            recipeRepository.save(recipe);
+            Mono<Recipe> recipeMono = recipeReactiveRepository
+                    .findById(recipeId)
+                    .flatMap(recipe -> {
+                        recipe.setImage(byteObjects);
+                        return recipeReactiveRepository.save(recipe);
+                    })
+                    .switchIfEmpty(Mono.defer(() -> {
+                        log.error("Recipe not found for id: " + recipeId);
+                        return Mono.empty();
+                    }));
+            return recipeMono;
         } catch (IOException e) {
             //todo handle better
             log.error("Error occurred", e);
 
             e.printStackTrace();
         }
+        return Mono.empty();
     }
 }
